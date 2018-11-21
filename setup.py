d16c6249bd59b8ef60c9ad69e1cba6c1b916cc42
ritchie46 <ritchie46@gmail.com>
2018-11-21 14:48:11 +0100

    add basic package to setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
     download_url='https://github.com/ritchie46/anaStruct',
     license='GPL-3.0',
     packages=['anastruct', 'anastruct.fem', 'anastruct.fem.system_components', 'anastruct.fem.examples',
-              'anastruct.material', 'anastruct.cython', 'anastruct.fem.cython', 'anastruct.fem.plotter'],
+              'anastruct.material', 'anastruct.cython', 'anastruct.fem.cython', 'anastruct.fem.plotter',
+              'anastruct.basic'],
     package_dir='',
     install_requires=[
         'matplotlib',
