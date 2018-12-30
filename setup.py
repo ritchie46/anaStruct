ca812fc1af3cf94194fcf72f003ac64bebceb59e
ritchie46 <ritchie46@gmail.com>
2018-12-30 12:40:06 +0100

    proper version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ if sys.version_info[0] == 3 and sys.version_info[1] < 5:
 setup(
     name='anastruct',
     version='1.0',
-    description='analyse 2D structures',
+    description='analyse 2D structures.',
     author='Ritchie Vink',
     author_email='ritchie46@gmail.com',
     url='https://ritchievink.com',
