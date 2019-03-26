402cd4f73ef6f803dd39a7049bfafb0213f5ef40
ritchie46 <ritchie46@gmail.com>
2019-03-26 09:24:44 +0100

    modules are more pythonic than dataclasses

---
+++ anastruct/sectionbase/units.py
@@ -0,0 +1,16 @@
+m = 1.
+cm = 1.e-2
+mm = 1.e-3
+kg = 1.
+N = 1.
+kN = 1.e3
+# IMPERIAL
+inch = 0.0254
+ft = 12. * inch
+lb = 0.4536
+lbf = 4.448
+kip = 1.e3 * lbf
+
+l_dict = {'m': m, 'cm': cm, 'mm': mm, 'ft': ft, 'inch': inch}
+m_dict = {'kg': kg, 'lb': lb}
+f_dict = {'N': N, 'kN': kN, 'lbf': lbf, 'kip': kip}
