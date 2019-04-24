f38a2ad1082867aba061ead8e48b61ac26850383
ritchie46 <ritchie46@gmail.com>
2019-04-24 10:02:02 +0200

    fix unittest section base

--- anastruct/sectionbase/sectionbase.py
+++ anastruct/sectionbase/sectionbase.py
@@ -10,13 +10,13 @@ class SectionBase:
     available_database_names = ['EU', 'US', 'UK']
 
     def __init__(self, basename='EU'):
-        self.out_lu = None
-        self.out_mu = None
-        self.out_fu = None
+        self.current_length_unit = None
+        self.current_mass_unit = None
+        self.current_force_unit = None
         self.current_database = None
-        self.xml_lu = None
-        self.xml_sdu = None
-        self.xml_wu = None
+        self.xml_length_unit = None
+        self.xml_area_unit = None
+        self.xml_weight_unit = None
         self.xml_self_weight_dead_load = None
         self.root = None  # xml root
 
@@ -24,16 +24,16 @@ class SectionBase:
         self.set_unit_system()
         self.load_data_from_xml()
 
-    def set_unit_system(self, lenght_unit='m', mass_unit='kg', force_unit='N'):
-        self.out_lu = u.l_dict[lenght_unit]
-        self.out_mu = u.m_dict[mass_unit]
-        self.out_fu = u.f_dict[force_unit]
+    def set_unit_system(self, length='m', mass_unit='kg', force_unit='N'):
+        self.current_length_unit = u.l_dict[length]
+        self.current_mass_unit = u.m_dict[mass_unit]
+        self.current_force_unit = u.f_dict[force_unit]
 
     def set_database_name(self, basename):
         if basename == 'EU' or basename == 'UK':
-            self.xml_lu = u.m
-            self.xml_sdu = u.m
-            self.xml_wu = u.kg
+            self.xml_length_unit = u.m
+            self.xml_area_unit = u.m
+            self.xml_weight_unit = u.kg
             self.xml_self_weight_dead_load = 10. * u.N
             if basename == 'EU':
                 self.current_database = 'sectionbase_EuropeanSectionDatabase.xml'
@@ -41,9 +41,9 @@ class SectionBase:
                 self.current_database = 'sectionbase_BritishSectionDatabase.xml'
         elif basename == 'US':
             self.current_database = 'sectionbase_AmericanSectionDatabase.xml'
-            self.xml_lu = u.ft
-            self.xml_sdu = u.inch
-            self.xml_wu = u.lb
+            self.xml_length_unit = u.ft
+            self.xml_area_unit = u.inch
+            self.xml_weight_unit = u.lb
             self.xml_self_weight_dead_load = u.lbf
 
         self.load_data_from_xml()
@@ -57,14 +57,13 @@ class SectionBase:
         )
         element['swdl'] = element['mass']
         element = self.convert_units(element)
-
         return element
 
     def convert_units(self, element):
-        lu = self.xml_lu / self.out_lu  # long unit
-        sdu = self.xml_sdu / self.out_lu  # sect dim unit
-        wu = self.xml_wu / self.out_mu  # weight unit
-        self_weight_dead_load = self.xml_self_weight_dead_load / self.out_fu  # self weight dead load unit
+        lu = self.xml_length_unit / self.current_length_unit  # long unit
+        sdu = self.xml_area_unit / self.current_length_unit  # sect dim unit
+        wu = self.xml_weight_unit / self.current_mass_unit  # weight unit
+        self_weight_dead_load = self.xml_self_weight_dead_load / self.current_force_unit  # self weight dead load unit
 
         element['mass'] = float(element['mass']) * wu / lu
         element['Ax'] = float(element['Ax']) * sdu ** 2
--- anastruct/sectionbase/test_sectionbase.py
+++ anastruct/sectionbase/test_sectionbase.py
@@ -4,11 +4,14 @@ from anastruct.fem import system
 
 
 class SimpleUnitTest(unittest.TestCase):
+    def setUp(self):
+        section_base_proxy().set_database_name('EU')
+        section_base_proxy().set_unit_system(length='m', mass_unit='kg', force_unit='N')
 
     def test_HE100B_EU(self):
-        # HEB 100 B has mass=20.4kg/m Ax=26cm2 Iy=449.5cm4 Iz=167.3cm4
+        # HEB 100 B has mass=20.4kg/m Ax=26.036cm2 Iy=449.5cm4 Iz=167.3cm4
 
-        section_base_proxy().set_unit_system(lenght_unit='m', mass_unit='kg', force_unit='N')
+        section_base_proxy().set_unit_system(length='m', mass_unit='kg', force_unit='N')
         param = section_base_proxy().get_section_parameters('HE 100 B')
         self.assertAlmostEqual(param['Ax'] / 26e-4, 1, places=2)
         self.assertAlmostEqual(param['mass'] / 20.4, 1, places=2)
@@ -16,7 +19,7 @@ class SimpleUnitTest(unittest.TestCase):
         self.assertAlmostEqual(param['Iz'] / 167.3e-8, 1, places=2)
         self.assertAlmostEqual(param['swdl'] / 20.4e1, 1, places=2)
 
-        section_base_proxy().set_unit_system(lenght_unit='cm', mass_unit='kg', force_unit='N')
+        section_base_proxy().set_unit_system(length='cm', mass_unit='kg', force_unit='N')
         param = section_base_proxy().get_section_parameters('HE 100 B')
         self.assertAlmostEqual(param['Ax'] / 26, 1, places=2)
         self.assertAlmostEqual(param['mass'] / 0.204, 1, places=2)
@@ -24,7 +27,7 @@ class SimpleUnitTest(unittest.TestCase):
         self.assertAlmostEqual(param['Iz'] / 167.3, 1, places=2)
         self.assertAlmostEqual(param['swdl'] / 0.204e1, 1, places=2)
 
-        section_base_proxy().set_unit_system(lenght_unit='mm', mass_unit='kg', force_unit='N')
+        section_base_proxy().set_unit_system(length='mm', mass_unit='kg', force_unit='N')
         param = section_base_proxy().get_section_parameters('HE 100 B')
         self.assertAlmostEqual(param['Ax'] / 26e2, 1, places=2)
         self.assertAlmostEqual(param['mass'] / 0.0204, 1, places=2)
@@ -32,7 +35,7 @@ class SimpleUnitTest(unittest.TestCase):
         self.assertAlmostEqual(param['Iz'] / 167.3e4, 1, places=2)
         self.assertAlmostEqual(param['swdl'] / 0.0204e1, 1, places=2)
 
-        section_base_proxy().set_unit_system(lenght_unit='ft', mass_unit='kg', force_unit='N')
+        section_base_proxy().set_unit_system(length='ft', mass_unit='kg', force_unit='N')
         param = section_base_proxy().get_section_parameters('HE 100 B')
         self.assertAlmostEqual(param['Ax'] / (26e-4 / (0.3048 ** 2)), 1, places=2)
         self.assertAlmostEqual(param['mass'] / (20.4 * 0.3048), 1, places=2)
@@ -40,7 +43,7 @@ class SimpleUnitTest(unittest.TestCase):
         self.assertAlmostEqual(param['Iz'] / (167.3e-8 / (0.3048) ** 4), 1, places=2)
         self.assertAlmostEqual(param['swdl'] / (20.4 * 0.3048 * 10), 1, places=2)
 
-        section_base_proxy().set_unit_system(lenght_unit='inch', mass_unit='kg', force_unit='N')
+        section_base_proxy().set_unit_system(length='inch', mass_unit='kg', force_unit='N')
         param = section_base_proxy().get_section_parameters('HE 100 B')
         self.assertAlmostEqual(param['Ax'] / (26e-4 / (0.0254 ** 2)), 1, places=2)
         self.assertAlmostEqual(param['mass'] / (20.4 * 0.0254), 1, places=2)
@@ -48,7 +51,7 @@ class SimpleUnitTest(unittest.TestCase):
         self.assertAlmostEqual(param['Iz'] / (167.3e-8 / (0.0254) ** 4), 1, places=2)
         self.assertAlmostEqual(param['swdl'] / (20.4 * 0.0254 * 10), 1, places=2)
 
-        section_base_proxy().set_unit_system(lenght_unit='ft', mass_unit='lb', force_unit='N')
+        section_base_proxy().set_unit_system(length='ft', mass_unit='lb', force_unit='N')
         param = section_base_proxy().get_section_parameters('HE 100 B')
         self.assertAlmostEqual(param['Ax'] / (26e-4 / (0.3048 ** 2)), 1, places=2)
         self.assertAlmostEqual(param['mass'] / (20.4 * 0.3048 / 0.454), 1, places=2)
@@ -56,7 +59,7 @@ class SimpleUnitTest(unittest.TestCase):
         self.assertAlmostEqual(param['Iz'] / (167.3e-8 / (0.3048) ** 4), 1, places=2)
         self.assertAlmostEqual(param['swdl'] / (20.4 * 0.3048 * 10), 1, places=2)
 
-        section_base_proxy().set_unit_system(lenght_unit='ft', mass_unit='lb', force_unit='lbf')
+        section_base_proxy().set_unit_system(length='ft', mass_unit='lb', force_unit='lbf')
         param = section_base_proxy().get_section_parameters('HE 100 B')
         self.assertAlmostEqual(param['Ax'] / (26e-4 / (0.3048 ** 2)), 1, places=2)
         self.assertAlmostEqual(param['mass'] / (20.4 * 0.3048 / 0.454), 1, places=2)
@@ -67,7 +70,7 @@ class SimpleUnitTest(unittest.TestCase):
     def test_W12X65_US(self):
         # W12X65 has mass=65[lb/ft] Ax=19.1[inch2] Iy=533[inch4] Iz=174[inch4]
         section_base_proxy().set_database_name('US')
-        section_base_proxy().set_unit_system(lenght_unit='inch', mass_unit='lb', force_unit='lbf')
+        section_base_proxy().set_unit_system(length='inch', mass_unit='lb', force_unit='lbf')
         param = section_base_proxy().get_section_parameters('W12X65')
         self.assertAlmostEqual(param['Ax'] / 19.1, 1, places=2)
         self.assertAlmostEqual(param['mass'] / (65 / 12), 1, places=2)
@@ -77,7 +80,6 @@ class SimpleUnitTest(unittest.TestCase):
 
     def test_sectionbase_steel_section_deflection(self):
         ss = system.SystemElements()
-        section_base_proxy().set_database_name('EU')
         ss.add_multiple_elements([[0, 0], [6, 0]], n=2, steelsection='IPE 300', E=210e9, sw=False, orient='y')
         ss.add_support_hinged(1)
         ss.add_support_hinged(3)
@@ -87,7 +89,6 @@ class SimpleUnitTest(unittest.TestCase):
 
     def test_sectionbase_steel_section_self_weight_reaction(self):
         ss = system.SystemElements()
-        section_base_proxy().set_database_name('EU')
         ss.add_element([[0, 0], [20, 0]], steelsection='IPE 300', sw=True)
         ss.add_support_hinged(1)
         ss.add_support_hinged(2)
@@ -96,7 +97,6 @@ class SimpleUnitTest(unittest.TestCase):
 
     def test_rectangle_section_deflection(self):
         ss = system.SystemElements()
-        section_base_proxy().set_database_name('EU')
         ss.add_multiple_elements([[0, 0], [2, 0]], n=2, h=0.05, b=0.16, E=210e9, sw=False, orient='y')
         ss.add_support_hinged(1)
         ss.add_support_hinged(3)
@@ -106,7 +106,6 @@ class SimpleUnitTest(unittest.TestCase):
 
     def test_circle_section_deflection(self):
         ss = system.SystemElements()
-        section_base_proxy().set_database_name('EU')
         ss.add_multiple_elements([[0, 0], [2, 0]], n=2, d=0.07, E=210e9, sw=False)
         ss.add_support_hinged(1)
         ss.add_support_hinged(3)
@@ -116,7 +115,6 @@ class SimpleUnitTest(unittest.TestCase):
 
     def test_rectangle_section_self_weight_reaction(self):
         ss = system.SystemElements()
-        section_base_proxy().set_database_name('EU')
         ss.add_element([[0, 0], [2, 0]], h=0.1, b=0.1, E=210e9, sw=True, gamma=10000)
         ss.add_support_hinged(1)
         ss.add_support_hinged(2)
@@ -125,7 +123,6 @@ class SimpleUnitTest(unittest.TestCase):
 
     def test_circle_section_self_weight_reaction(self):
         ss = system.SystemElements()
-        section_base_proxy().set_database_name('EU')
         ss.add_element([[0, 0], [2, 0]], d=0.2, E=210e9, sw=True, gamma=10000)
         ss.add_support_hinged(1)
         ss.add_support_hinged(2)
