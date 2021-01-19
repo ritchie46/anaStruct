b574206150ea0d6c99d06b6f5e9aaad5d6d25079
Brooks Smith <brooks.smith@clearcalcs.com>
2021-01-19 15:00:33 +1100

    Don't apply load_factors in assembly step for any load types

--- anastruct/fem/system_components/assembly.py
+++ anastruct/fem/system_components/assembly.py
@@ -39,7 +39,7 @@ def prep_matrix_forces(system: "SystemElements"):
 
 def apply_moment_load(system: "SystemElements"):
     for node_id, Ty in system.loads_moment.items():
-        set_force_vector(system, [(node_id, 3, Ty * system.load_factor)])
+        set_force_vector(system, [(node_id, 3, Ty)])
 
 
 def apply_point_load(system: "SystemElements"):
@@ -49,8 +49,8 @@ def apply_point_load(system: "SystemElements"):
         set_force_vector(
             system,
             [
-                (node_id, 1, Fx * system.load_factor),
-                (node_id, 2, Fz * system.load_factor),
+                (node_id, 1, Fx),
+                (node_id, 2, Fz),
             ],
         )
 
