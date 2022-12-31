28d6e8693f22f6066cdb92b187add4cedf5455ec
Brooks Smith <brooks.smith@clearcalcs.com>
2022-12-31 17:12:33 +1100

    Fix Q-loads in LoadCases, where an incorrect data structure was specified

--- anastruct/fem/util/load.py
+++ anastruct/fem/util/load.py
@@ -1,5 +1,6 @@
 import pprint
 import copy
+from typing import Sequence
 from anastruct.basic import args_to_lists
 
 
@@ -26,10 +27,10 @@ class LoadCase:
         """
         if q_perp is None:
             q_perp = [0, 0]
-        if not isinstance(q, tuple):
-            q = [(q, q)]
-        if not isinstance(q_perp, tuple):
-            q_perp = [(q_perp, q_perp)]
+        if not isinstance(q, Sequence):
+            q = [q, q]
+        if not isinstance(q_perp, Sequence):
+            q_perp = [q_perp, q_perp]
         q = [q]
         q_perp = [q_perp]
         self.c += 1
