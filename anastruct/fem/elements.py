7a563f1d82bed070d8cdc6268f164185b3bae648
Brooks Smith <brooks.smith@clearcalcs.com>
2023-01-11 20:53:17 +1100

    Handle null deflections

--- anastruct/fem/elements.py
+++ anastruct/fem/elements.py
@@ -215,7 +215,9 @@ class Element:
                 setattr(el, unit, getattr(other, unit))
             else:
                 setattr(el, unit, getattr(el, unit) + getattr(other, unit))
-        el.max_deflection = np.max(np.abs(el.deflection))
+        el.max_deflection = (
+            None if el.deflection is None else np.max(np.abs(el.deflection))
+        )
 
         el.node_map[self.node_id1] = el.node_1 + other.node_1
         el.node_map[self.node_id2] = el.node_2 + other.node_2
