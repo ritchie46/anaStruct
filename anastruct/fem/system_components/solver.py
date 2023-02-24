0ea75b62351f1b6d829bffd0835599ad0a47a3f3
Brooks Smith <brooks.smith@clearcalcs.com>
2023-02-24 11:46:33 +1100

    Remove scipy dependency by replacing eigvals() function

--- anastruct/fem/system_components/solver.py
+++ anastruct/fem/system_components/solver.py
@@ -2,7 +2,6 @@ import copy
 import logging
 from typing import TYPE_CHECKING, Optional
 import numpy as np
-from scipy import linalg  # type: ignore
 from anastruct.basic import converge
 
 
@@ -109,7 +108,8 @@ def det_linear_buckling(system: "SystemElements") -> float:
     kg = system.reduced_system_matrix - k0
     # solve (k -λkg)x = 0
 
-    eigenvalues = np.abs(linalg.eigvals(k0, kg))
+    invkg_k0 = np.linalg.inv(kg) * k0
+    eigenvalues = np.abs(np.linalg.eigvals(invkg_k0))
     return float(np.min(eigenvalues))
 
 
