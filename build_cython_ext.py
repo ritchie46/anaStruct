96603dc58310d2bfe1eeb0174dd9a8365b5cf687
smith120bh <42363318+smith120bh@users.noreply.github.com>
2024-07-20 17:50:43 +1000

    Swap to Poetry as a package manager

---
+++ build_cython_ext.py
@@ -0,0 +1,29 @@
+from Cython.distutils import build_ext
+
+# from Cython.Build import cythonize
+from setuptools import Extension
+
+define_macros = [("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")]
+
+ext_modules = [
+    Extension(
+        "anastruct.fem.cython",
+        sources=["anastruct/cython/cbasic.pyx", "anastruct/fem/cython/celements.pyx"],
+        define_macros=define_macros,
+    ),
+]
+
+
+class BuildExt(build_ext):
+    def build_extensions(self) -> None:
+        for ext in ext_modules:
+            self.build_extension(ext)
+
+
+def build(setup_kwargs: dict) -> None:
+    setup_kwargs.update(
+        {
+            "ext_modules": ext_modules,
+            "cmdclass": {"build_ext": BuildExt},
+        }
+    )
