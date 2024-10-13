ffc35ccbd433bca07bdbb8b22ca6d699a097b737
unknown <bldrkamal@gmail.com>
2024-10-13 18:38:54 +0100

    Added get_stiffness_matrix method to SystemElements and corresponding test_stifness

--- getst.py
+++
@@ -1,9 +0,0 @@
-
-from anastruct.fem.system import SystemElements
-ss=SystemElements()
-
-# Add an element to the system (Example: Beam element between two nodes)
-ss.add_element(location=[[0, 0], [5, 0]])  # Simple beam element
-
-
-ss.get_stiffness_matrix(1)  # Element ID 1
