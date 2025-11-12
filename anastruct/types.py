7cf7044d75fda5692b38756d5e72b74008e83d1c
Brooks Smith <42363318+smith120bh@users.noreply.github.com>
2025-11-12 21:22:37 +1100

    Add SectionProps type, always import Vertex

--- anastruct/types.py
+++ anastruct/types.py
@@ -1,9 +1,8 @@
-from typing import TYPE_CHECKING, Dict, Literal, Sequence, Union
+from typing import TYPE_CHECKING, Dict, Literal, Sequence, TypedDict, Union
 
 import numpy as np
 
-if TYPE_CHECKING:
-    from anastruct.vertex import Vertex
+from anastruct.vertex import Vertex
 
 AxisNumber = Literal[1, 2, 3]
 Dimension = Literal["x", "y", "y_neg", "both"]
@@ -15,3 +14,12 @@ OrientAxis = Literal["y", "z"]
 Spring = Dict[Literal[1, 2], float]
 SupportDirection = Literal["x", "y", "1", "2", 1, 2]
 VertexLike = Union[Sequence[Union[float, int]], np.ndarray, "Vertex"]
+
+SectionProps = TypedDict(
+    "SectionProps",
+    {
+        "EI": float,
+        "EA": float,
+        "g": float,
+    },
+)
